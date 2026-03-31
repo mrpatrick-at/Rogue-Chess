c1e48db73d8016f2608be8685e4c2ff5879877d9
mrpatrick-at <70460035+mrpatrick-at@users.noreply.github.com>
2026-03-31 23:36:36 +0200

    General Cleaning of Code

--- chess_objects/board/tilemap_layer.gd
+++ chess_objects/board/tilemap_layer.gd
@@ -18,40 +18,49 @@ func _process(_delta: float) -> void:
 ## public methods
 
 func build_board() -> void:
-	var source_id:int = 0
 	for x in range(1,9):
 		for y in range(1,9):
 			calculate_tile_color(x,y)
-			var atlas_coords:Vector2i = Vector2i(Scripts.BOARD_DATABASE.TILE_DICTIONARY.get(Vector2i(x,y)),0) # White: 0,0 ; Black: 1,0 ; Selection_Sprite: 2,0 ;
+			var source_id:int = 0
 			var coords:Vector2i = Vector2i(x,y) # Black bottom left is 1,1
+			var atlas_coords:Vector2i = Vector2i(Scripts.BOARD_DATABASE.TILE_DICTIONARY.get(coords),0) # Blank: 0,0 ; White: 1,0 ; Black: 2,0 ; Selection_Sprite: 3,0 ;
 			set_cell(coords,source_id,atlas_coords,source_id)
 
 static func calculate_tile_color(x:int,y:int) -> void:
-	var int_x:int = 0 # Check if x is odd or even
-	if (x & 1) == 0:
-		int_x += 2
-	if (x & 1) == 1:
-		int_x += 1
+	if !Scripts.BOARD_DATABASE.TILE_DICTIONARY.has(Vector2i(x,y)):
+		var int_x:int = 0 # Check if x is odd or even
+		if (x & 1) == 0:
+			int_x += 2
+		if (x & 1) == 1:
+			int_x += 1
+			
+		var int_y:int = 0 # Check if y is odd or even
+		if (y & 1) == 0:
+			int_y += 2
+		if (y & 1) == 1:
+			int_y += 1
 		
-	var int_y:int = 0 # Check if y is odd or even
-	if (y & 1) == 0:
-		int_y += 2
-	if (y & 1) == 1:
-		int_y += 1
+		var colour_of_tile:String = "unkown"
+		var same_or_diffrent:int = 0
+		if int_x == 2 and int_y == 2 or int_x == 1 and int_y == 1:
+			same_or_diffrent = 1
+		else:
+			same_or_diffrent = 2
+		if same_or_diffrent == 1: # Values same = black tile
+			Scripts.BOARD_DATABASE.TILE_BLACK += 1
+			Scripts.BOARD_DATABASE.TILE_DICTIONARY.get_or_add(Vector2i(x,y),1)
+			# TODO: Make the Data storage into a dictionary inside a dictionary to hold more than 1 value
+			#Scripts.BOARD_DATABASE.TILE_DICTIONARY.get_or_add(Vector2i(x,y){tile_colour: 1,gay_colour: 2,})
+			colour_of_tile = "White"
 		
-	if int_x == 2 and int_y == 2 or int_x == 1 and int_y == 1: # Values same = black tile
-		Scripts.BOARD_DATABASE.TILE_BLACK += 1
-		Scripts.BOARD_DATABASE.TILE_DICTIONARY.get_or_add(Vector2i(x,y),0)
-		print("Black Tile Found: ",x,",",y," Now there is ",Scripts.BOARD_DATABASE.TILE_BLACK,"/32 black tiles!")
-	
-	if int_x == 2 and int_y == 1 or int_x == 1 and int_y == 2: # values diffrent = white tile
-		Scripts.BOARD_DATABASE.TILE_WHITE += 1
-		Scripts.BOARD_DATABASE.TILE_DICTIONARY.get_or_add(Vector2i(x,y),1)
-		print("White Tile Found: ",x,",",y," Now there is ",Scripts.BOARD_DATABASE.TILE_WHITE,"/32 white tiles!")
+		if same_or_diffrent == 2: # values diffrent = white tile
+			Scripts.BOARD_DATABASE.TILE_WHITE += 1
+			Scripts.BOARD_DATABASE.TILE_DICTIONARY.get_or_add(Vector2i(x,y),2)
+			colour_of_tile = "Black"
 		
-	Scripts.BOARD_DATABASE.TOTAL_TILES = Scripts.BOARD_DATABASE.TILE_BLACK + Scripts.BOARD_DATABASE.TILE_WHITE # Total Ints (Should be 64 for Chess board)
-	print("Total Tiles: ",Scripts.BOARD_DATABASE.TOTAL_TILES,"/64 !")
-	return Vector2i(x,y)
+		Scripts.BOARD_DATABASE.TOTAL_TILES = Scripts.BOARD_DATABASE.TILE_BLACK + Scripts.BOARD_DATABASE.TILE_WHITE # Total Ints (Should be 64 for Chess board)
+		print(colour_of_tile," Tile Found at: ",x,",",y," ; Now there is ",Scripts.BOARD_DATABASE.TOTAL_TILES,"/64 tiles!")
+		return Vector2i(x,y)
 
 func create_board_cells(coords:Vector2i,source_id:int,atlas_coords:Vector2i) -> void:
 	set_cell(coords,source_id,atlas_coords,source_id)
