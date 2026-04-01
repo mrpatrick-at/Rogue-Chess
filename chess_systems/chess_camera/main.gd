53fd21357cbe507b9939e0950a9f732afcc1055d
mrpatrick-at <70460035+mrpatrick-at@users.noreply.github.com>
2026-04-01 23:55:16 +0200

    Cleaned Up Player Script

--- chess_systems/chess_camera/main.gd
+++ chess_systems/chess_camera/main.gd
@@ -1,11 +1,16 @@
-extends Camera2D
+extends Node2D
 ## enums
 ## consts
+const CAMERA_ZOOM_SPEED:float = 4.0
+const CAMERA_ZOOM_RANGE:Vector2 = Vector2(0.1,2.5)
+const CAMERA_MOVE_SPEED:Vector2 = Vector2(0.1,2.5)
 ## exports
 ## public vars
-@onready var camera_2d: Camera2D = $"."
+static var cam_movement_velocity:Vector2
+static var cam_zoom_velocity:float = 0.0
 ## private vars
 ## onready vars
+@onready var cam: Camera2D = $Camera2D
 # obj_ for node refrences
 ## built-in override methods
 
@@ -13,30 +18,37 @@ func _ready() -> void:
 	pass 
 
 func _physics_process(delta: float) -> void:
-	move_camera()
+	_apply_camera_movement()
+	_apply_camera_zoom()
 	pass
 
 ## public methods
 
-func move_camera() -> void:
-	var delta = get_process_delta_time()
-	if Input.is_action_pressed("_input_up"):
-		#print("up")
-		position.y -= 4000 * delta
-	if Input.is_action_pressed("_input_down"):
-		#print("down")
-		position.y += 4000 * delta
-	if Input.is_action_pressed("_input_left"):
-		#print("left")
-		position.x -= 4000 * delta
-	if Input.is_action_pressed("_input_right"):
-		#print("right")
-		position.x += 4000 * delta
-	if Input.is_action_just_released(&"_input_mouse_scroll_up"):
-		#print("scroll up")
-		zoom += Vector2(2,2) * delta
-	if Input.is_action_just_released(&"_input_mouse_scroll_down"):
-		#print("scroll down")
-		zoom -= Vector2(2,2) * delta
+static func move_camera(direction:Vector2,delta:float) -> void:
+	cam_movement_velocity.x = direction.x * delta * 2000
+	cam_movement_velocity.y = direction.y * delta * 2000
+
+static func zoom_camera(direction:int,delta:float) -> void:
+	cam_zoom_velocity += ((CAMERA_ZOOM_SPEED)* delta) * direction
 
 ## private methods
+
+func _apply_camera_movement() -> void:
+	if cam_movement_velocity != Vector2.ZERO:
+		var camer_zoom_speed:float = remap(
+			cam.zoom.x,
+			CAMERA_ZOOM_RANGE.x,CAMERA_ZOOM_RANGE.y,
+			CAMERA_ZOOM_RANGE.y,CAMERA_ZOOM_RANGE.x)
+		print("real zoom: ",cam.zoom,"remaped zoom: ",camer_zoom_speed)
+		cam.position.x += cam_movement_velocity.x * camer_zoom_speed
+		cam.position.y += cam_movement_velocity.y * camer_zoom_speed
+		cam_movement_velocity = Vector2.ZERO
+
+func _apply_camera_zoom() -> void:
+	if cam_zoom_velocity != 0:
+		var calculated_zoom:float = cam.zoom.x + cam_zoom_velocity
+		if (calculated_zoom > CAMERA_ZOOM_RANGE.x) and (calculated_zoom < CAMERA_ZOOM_RANGE.y):
+			cam.zoom.x += cam_zoom_velocity
+			cam.zoom.y += cam_zoom_velocity
+	cam_zoom_velocity = 0
+	pass
--- chess_systems/chess_player/main.gd
+++ chess_systems/chess_player/main.gd
@@ -15,16 +15,41 @@ func _ready() -> void:
 	
 	pass 
 
-func _input(event:InputEvent) -> void:
+func _physics_process(delta: float) -> void:
+	_mouse_buttons(delta)
+	_camera_movement(delta)
+	_camera_zoom(delta)
+
+func _mouse_buttons(delta) -> void:
 	# Mouse Inputs
 	if InputEventMouse:
 		var mouse_coords:Vector2 = get_viewport().get_mouse_position()
-		#if Input.is_action_just_released(&"_input_mouse_left"):
-			#print("Left Mouse click detected at: ",mouse_coords)
+		if Input.is_action_just_released(&"_input_mouse_left"):
+			print("Left Mouse click detected at: ",mouse_coords)
 		if Input.is_action_just_released(&"_input_mouse_right"):
 			print("Right Mouse click detected at: ",mouse_coords)
 		if Input.is_action_just_released(&"_input_mouse_middle"):
 			print("Middle Mouse click detected at: ",mouse_coords)
+
+func _camera_movement(delta) -> void:
+	# Camera Movement
+	var direction:Vector2 = Vector2.ZERO
+	if Input.is_action_pressed(&"_input_up") and !Input.is_action_pressed(&"_input_down"): direction.y = -1
+	if Input.is_action_pressed(&"_input_down") and !Input.is_action_pressed(&"_input_up"): direction.y = +1 
+	if Input.is_action_pressed(&"_input_left") and !Input.is_action_pressed(&"_input_right"): direction.x = -1 
+	if Input.is_action_pressed(&"_input_right") and !Input.is_action_pressed(&"_input_left"): direction.x = +1
+	if direction == Vector2.ZERO:return # No movement
+	Scripts.CHESS_CAMERA2D.move_camera(direction,delta)
+
+func _camera_zoom(delta) -> void:
+	# Camera Zoom
+	var direction:float = 0
+	if Input.is_action_just_released(&"_input_mouse_scroll_up"):
+		direction = 1
+	if Input.is_action_just_released(&"_input_mouse_scroll_down"):
+		direction = -1
+	if !direction:return
+	Scripts.CHESS_CAMERA2D.zoom_camera(direction,delta)
 
 ## public methods
 
