f6a67f6260a1d7bcf03a92c8c91af545310dd5e9
mrpatrick-at <70460035+mrpatrick-at@users.noreply.github.com>
2026-04-01 04:59:14 +0200

    Switched to Dictionary Data Storage, so I can store more Data per coord

--- chess_objects/board/tilemap_layer.gd
+++ chess_objects/board/tilemap_layer.gd
@@ -20,12 +20,11 @@ func _process(_delta: float) -> void:
 func build_board(x_range:int,y_range:int) -> void: # Remember y_range needs to be +1 bc it stops 1 before
 	for x in range(1,x_range):
 		for y in range(1,y_range):
-			if !Scripts.BOARD_DATABASE.TILE_DICTIONARY.has(Vector2i(x,y)):
-				var coords:Vector2i = Vector2i(x,y)
+			var coords:Vector2i = Vector2i(x,y)
+			if !Scripts.BOARD_DATABASE.TILE_DICTIONARY.has(coords):
 				calculate_tile_color(coords)
-				var source_id:int = 0 # Black bottom left is 1,1
-				var atlas_coords:Vector2i = Vector2i(Scripts.BOARD_DATABASE.TILE_DICTIONARY.get(coords),0) # Blank: 0,0 ; White: 1,0 ; Black: 2,0 ; Selection_Sprite: 3,0 ;
-				set_cell(coords,source_id,atlas_coords,source_id)
+				var source_id:int = 0 # Value Controls Layer of which board is rendered at. Can be used to overlay multiple Layers
+				create_board_cells(source_id,coords)
 
 static func calculate_tile_color(coords:Vector2i) -> void:
 	var int_x:int = 0 # Check if x is odd or even
@@ -50,21 +49,19 @@ static func calculate_tile_color(coords:Vector2i) -> void:
 		
 	if same_or_diffrent == 1: # Values same = black tile
 		Scripts.BOARD_DATABASE.TILE_BLACK += 1
-		Scripts.BOARD_DATABASE.TILE_DICTIONARY.get_or_add(coords,1)
-		# TODO: Make the Data storage into a dictionary inside a dictionary to hold more than 1 value
-		#Scripts.BOARD_DATABASE.TILE_DICTIONARY.get_or_add(Vector2i(x,y){tile_colour: 1,gay_colour: 2,})
+		Scripts.BOARD_DATABASE.TILE_DICTIONARY.get_or_add(coords,{"colour":1,"test":"gay"})
 		colour_of_tile = "White"
 	
 	if same_or_diffrent == 2: # values diffrent = white tile
 		Scripts.BOARD_DATABASE.TILE_WHITE += 1
-		Scripts.BOARD_DATABASE.TILE_DICTIONARY.get_or_add(coords,2)
+		Scripts.BOARD_DATABASE.TILE_DICTIONARY.get_or_add(coords,{"colour":2,"test":"gay"})
 		colour_of_tile = "Black"
-	
+		
 	Scripts.BOARD_DATABASE.TOTAL_TILES = Scripts.BOARD_DATABASE.TILE_BLACK + Scripts.BOARD_DATABASE.TILE_WHITE # Total Ints (Should be 64 for Chess board)
 	print(colour_of_tile," Tile Found at: ",coords," ; Now there is ",Scripts.BOARD_DATABASE.TOTAL_TILES,"/64 tiles!")
 	return
 
-func create_board_cells(coords:Vector2i,source_id:int,atlas_coords:Vector2i) -> void:
+func create_board_cells(source_id:int,coords:Vector2i) -> void:
+	var atlas_coords:Vector2i = Vector2i(Scripts.BOARD_DATABASE.TILE_DICTIONARY[coords]["colour"],0) # Blank: 0,0 ; White: 1,0 ; Black: 2,0 ; Selection_Sprite: 3,0 ;
 	set_cell(coords,source_id,atlas_coords,source_id)
-
 ## private methods
