47d8d7453a4d6e05d87ac886e9c173586d11994f
leo <70460035+mrpatrick-at@users.noreply.github.com>
2026-04-14 23:10:00 +0200

    Basic Main Menu Concept

---
+++ chess_systems/chess_ui/main_menu.gd
@@ -0,0 +1,28 @@
+extends Control
+## enums
+## consts
+## exports
+## public vars
+## private vars
+## onready vars
+# obj_ for node refrences
+## built-in override methods
+
+func _ready() -> void:
+	pass 
+
+func _process(_delta: float) -> void:
+	pass
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+## public methods
+
+## private methods
